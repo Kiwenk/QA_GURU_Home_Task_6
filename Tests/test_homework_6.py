7b5509e55ff933dff10516ed65d06aa047c8c794
Katsuba Igor <kasubaigor@gmail.com>
2024-06-27 14:43:26 +0300

    Домашняя работа 6

--- Tests/test_homework_6.py
+++ Tests/test_homework_6.py
@@ -26,7 +26,7 @@ def test_dark_theme_by_time_and_user_choice():
     dark_theme_enabled_by_user = True
 
     if dark_theme_enabled_by_user is True:
-        if time(hour=22) <= current_time <= time(hour=23) or time(hour=0) <= current_time <= time(hour=6):
+        if time(hour=22) <= current_time or current_time < time(hour=6):
             is_dark_theme = True
         else:
             is_dark_theme = False
